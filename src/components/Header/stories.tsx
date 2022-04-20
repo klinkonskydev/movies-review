96ff25fb6645dea10a3deea514e12f190840f3d5
klinkonskydev <klinkonskydev@gmail.com>
2022-04-20 16:52:55 -0300

    fix: identation

--- src/components/Header/stories.tsx
+++ src/components/Header/stories.tsx
@@ -2,13 +2,13 @@ import { Meta } from '@storybook/react'
 import Header from '.'
 
 export default {
-    title: 'Header',
-    component: Header,
-    parameters: {
-        backgrounds: {
-            default: 'dark'
-        }
+  title: 'Header',
+  component: Header,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
     }
+  }
 } as Meta
 
 export const Default = () => <Header />
@@ -16,7 +16,7 @@ export const Default = () => <Header />
 export const Mobile = () => <Header />
 
 Mobile.parameters = {
-    viewport: {
-        defaultViewport: 'mobile1'
-    },
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
 }
