91dc1fa7ccdc0d6ce19e1d53fcce29773f04ddd6
klinkonskydev <klinkonskydev@gmail.com>
2022-04-20 11:15:10 -0300

    feat: header styles

--- src/components/Header/test.tsx
+++ src/components/Header/test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { render } from 'utils/render'
 import Header from '.'
 
 jest.mock('next/image', () => ({
   __esModule: true,
-  default: () => "/logo.svg"
+  default: () => '/logo.svg'
 }))
 
 describe('<Header />', () => {
@@ -11,5 +12,17 @@ describe('<Header />', () => {
     render(<Header />)
 
     expect(screen.getByRole('link')).toHaveTextContent('/logo.svg')
+
+    expect(screen.getByRole('link').parentElement).toHaveStyle({
+      'justify-content': 'center'
+    })
+
+    expect(screen.getByRole('link').parentElement).toHaveStyleRule(
+      'justify-content',
+      'flex-start',
+      {
+        media: '(min-width: 768px)'
+      }
+    )
   })
 })
