4a02e28aab489cc663fd55ec269a6da2e93c0551
klinkonskydev <klinkonskydev@gmail.com>
2022-04-23 16:35:08 -0300

    fix: styled-coponents unnecessary config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,33 +6,18 @@ import Document, {
   NextScript
 } from 'next/document'
 
-import { ServerStyleSheet } from 'styled-components'
-
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        })
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component
+      })
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      }
-    } finally {
-      sheet.seal()
-    }
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
   }
 
   render() {
