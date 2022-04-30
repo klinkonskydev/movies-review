8148a4b55642b7fcb3966dc11655fed364b4134a
klinkonskydev <klinkonskydev@gmail.com>
2022-04-30 20:14:10 -0300

    feat - to movie page

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import * as S from './styles'
 
 export type CardProps = {
@@ -8,17 +9,25 @@ export type CardProps = {
   type?: 'author' | 'movie'
 }
 
-const Card = ({ poster_path, title, subtitle, type = 'movie' }: CardProps) => (
-  <S.Wrapper type={type}>
-    <S.Image
-      src={`https://image.tmdb.org/t/p/original${poster_path}`}
-      role="img"
-      aria-label={type === 'movie' ? `${title} movie` : `${title} author`}
-      loading="lazy"
-    />
+const Card = ({
+  id,
+  poster_path,
+  title,
+  subtitle,
+  type = 'movie'
+}: CardProps) => (
+  <Link href={`/${id}`}>
+    <S.Wrapper type={type}>
+      <S.Image
+        src={`https://image.tmdb.org/t/p/original${poster_path}`}
+        role="img"
+        aria-label={type === 'movie' ? `${title} movie` : `${title} author`}
+        loading="lazy"
+      />
 
-    <S.Heading>{title}</S.Heading>
-    <S.Subtitle>{subtitle}</S.Subtitle>
-  </S.Wrapper>
+      <S.Heading>{title}</S.Heading>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+    </S.Wrapper>
+  </Link>
 )
 export default Card
