33b33426d9abd71acbd46b36c04e78b29e33f5bc
klinkonskydev <klinkonskydev@gmail.com>
2022-05-01 13:21:57 -0300

    feat - Next progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import NextNProgress from 'nextjs-progressbar'
+
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from 'styles/global'
 import theme from 'styles/theme'
@@ -12,6 +14,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Movies Review</title>
         <meta name="theme-color" content="#5C16C5" />
       </Head>
+
+      <NextNProgress
+        color={theme.colors.green}
+        height={5}
+      />
+
       <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
