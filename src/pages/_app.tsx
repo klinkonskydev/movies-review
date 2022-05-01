6086d8c40596d3fde2aaa865f64b6d67e830e510
klinkonskydev <klinkonskydev@gmail.com>
2022-05-01 10:04:49 -0300

    feat - title & meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from 'styles/global'
@@ -7,6 +8,10 @@ import theme from 'styles/theme'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Movies Review</title>
+        <meta name="theme-color" content="#5C16C5" />
+      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
